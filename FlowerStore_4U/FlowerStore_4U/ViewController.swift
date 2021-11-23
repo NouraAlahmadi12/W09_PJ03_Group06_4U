85464c81c3c2b1e4158737e21dcc6d806b25adc1
Noura.alahmadi.med.s@tuwaiq.edu.sa <Teeko1122>
2021-11-23 20:33:15 +0300

    (@NoraAlhamdi12):  - Create Model contain ( struct of FlowersInfo & class of FlowersList )  - Struct FlowersInfo contain the flowers Information  - Class FlowersList contain array of flowerList of type FlowersInfo & func to add items to the array  - Create coca touch file for the collectionViewCell contain (Image , Label for name , Label for price)  - Fill the Array With the products
     (@iiRazan):
     - Design App icon Logo & icon of costume Button & icon of sort Button
     - Cell Design
    
    (@AfafSaleh55):
    - Constrains

--- FlowerStore_4U/FlowerStore_4U/ViewController.swift
+++ FlowerStore_4U/FlowerStore_4U/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource{
+    
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    var items = FlowersList()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.flowerList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProuductID", for: indexPath) as! ProuductsCell
+        
+        cell.imgOfFlower.image = items.flowerList[indexPath.row].flowerImage
+        cell.labelName.text = items.flowerList[indexPath.row].flowerName
+        cell.labelPrice.text = String(items.flowerList[indexPath.row].flowerPrice)
+        cell.imgOfFlower.layer.cornerRadius = 30
+        return cell
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        items.addItem(newItem: FlowersInfo(flowerName: "Orang Bouqeut", flowerDetails: "Orange Flowers with Flowers Height 100 CM Width 50 CM", flowerImage: UIImage(named: "OrangeFlower")!, flowerPrice: 121))
+        
+        items.addItem(newItem: FlowersInfo(flowerName: "Blue Bouqeut", flowerDetails: "Blue Flowers Height 86 CM Width 24 CM", flowerImage: UIImage(named: "BlueFlower")!, flowerPrice: 256))
+        
+        items.addItem(newItem: FlowersInfo(flowerName: "Pink Bouqeut", flowerDetails: "Pink Flowers Height 57 CM Width 60 CM", flowerImage: UIImage(named: "PinkFlower")!, flowerPrice: 162.50))
+        
+        items.addItem(newItem: FlowersInfo(flowerName: "Red Bouqeut", flowerDetails: "Red , black and white Flowers with Height 76 CM Width 47 CM", flowerImage: UIImage(named: "RedFlower")!, flowerPrice: 97.92))
+        
+        items.addItem(newItem: FlowersInfo(flowerName: "White Bouqeut", flowerDetails: "white Flowers Height 72 CM Width 60 CM", flowerImage: UIImage(named: "WhiteFlower")!, flowerPrice: 90.92))
+        
+        
+        items.addItem(newItem: FlowersInfo(flowerName: "sun Bouqeut", flowerDetails: "sun Flowers Height 60 CM Width 15 CM", flowerImage: UIImage(named: "sunFlower")!, flowerPrice: 100))
+        
+        
+        itemsCollectionView.delegate = self
+        itemsCollectionView.dataSource = self
     }
-
-
 }
 
