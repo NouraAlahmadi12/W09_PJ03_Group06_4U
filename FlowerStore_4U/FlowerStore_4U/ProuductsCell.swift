ac99a9fb3c6e0ae1b818acd03d855730de271a1e
NouraAlahmadi12 <91524062+NouraAlahmadi12@users.noreply.github.com>
2021-11-23 20:41:32 +0300

    (@NoraAlhamdi12): (#1)
    - Create Model contain ( struct of FlowersInfo & class of FlowersList )
     - Struct FlowersInfo contain the flowers Information
     - Class FlowersList contain array of flowerList of type FlowersInfo & func to add items to the array
     - Create coca touch file for the collectionViewCell contain (Image , Label for name , Label for price)
     - Fill the Array With the products
    
     (@iiRazan):
     - Design App icon Logo & icon of costume Button & icon of sort Button
     - Cell Design
    
    (@AfafSaleh55):
    - Constrains
    
    Co-authored-by: Noura.alahmadi.med.s@tuwaiq.edu.sa <Teeko1122>

---
+++ FlowerStore_4U/FlowerStore_4U/ProuductsCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProuductsCell.swift
+//  FlowerStore_4U
+//
+//  Created by Noura Alahmadi on 17/04/1443 AH.
+//
+
+import UIKit
+
+class ProuductsCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgOfFlower: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelPrice: UILabel!
+}
