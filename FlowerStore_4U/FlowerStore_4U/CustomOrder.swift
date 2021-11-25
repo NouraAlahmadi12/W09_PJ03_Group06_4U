ab029c7d15936c1bd34057c683d2d9e5abc67758
AfafSaleh55 <91525724+AfafSaleh55@users.noreply.github.com>
2021-11-25 14:14:57 +0300

    Camera and Library (#7)
    - Create func for showPhotoAlert() -> alert to access photo library and camera & cancel.
    - Create func to getPhoto() -> to edit .
    - Create func did Finish Picking Media With Info -> to take Photo and show message.

---
+++ FlowerStore_4U/FlowerStore_4U/CustomOrder.swift
@@ -0,0 +1,78 @@
+//
+//  CustomOrder.swift
+//  FlowerStore_4U
+//
+//  Created by Noura Alahmadi on 20/04/1443 AH.
+//
+
+import UIKit
+
+class CustomOrder: UIViewController , UIPickerViewDelegate , UIPickerViewDataSource, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
+    
+    let roes = ["jouri roses - Red" , "jouri roses - White" , "jouri roses - Yellow"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return roes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return roes[row]
+    }
+    
+    func showPhotoAlert(){
+        let alert = UIAlertController(title: "Take Photo From:", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { action in
+            self.getPhoto(type: .camera)
+        }))
+        alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { action in
+                self.getPhoto(type: .photoLibrary)
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    present(alert, animated: true, completion: nil)
+    
+    }
+    func getPhoto(type : UIImagePickerController.SourceType){
+        let picker = UIImagePickerController()
+        picker.sourceType = type
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+dismiss(animated: true, completion: nil)
+        guard let imageCL = info[.originalImage] as? UIImage else {
+            print("Image not Found")
+            return
+        }
+        imageFromCL.image = imageCL
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var imageFromCL: UIImageView!
+    @IBOutlet weak var chooseRose: UIPickerView!
+    @IBAction func tapOnImage(_ sender: Any) {
+        showPhotoAlert()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        chooseRose.dataSource = self
+        chooseRose.delegate = self
+     
+    }
+    
+    
+
+    
+}
+
+
