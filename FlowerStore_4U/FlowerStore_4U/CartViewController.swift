d7fab51543241fa126dbc9be4fd62905da937444
AfafSaleh <Afaf.Saleh.med.s@tuwaiq.edu.sa>
2021-11-28 01:19:20 +0300

    ...
    - Create Func trailingSwipeActionsConfigurationForRowAt to delete swipe and delete all Data from CartViewController and delete data from CoreData.
    - Create Func cleanDB to clean data.

--- FlowerStore_4U/FlowerStore_4U/CartViewController.swift
+++ FlowerStore_4U/FlowerStore_4U/CartViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class CartViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -16,7 +17,7 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {return cartOrder.count}
-   
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = cartItemTableView.dequeueReusableCell(withIdentifier: "cartCellID") as! CartTableCell
@@ -24,12 +25,21 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.nameInCart.text = cartOrder[indexPath.row].flowerName
         cell.priceInCart.text = String(cartOrder[indexPath.row].flowerPrice)
         cell.imageInCart.image = UIImage(named:cartOrder[indexPath.row].flowerImage ?? " ")
-       return cell
+        return cell
     }
-
+    
     @IBAction func deleteAll(_ sender: Any) {
         cartOrder.removeAll()
-        
+        do {
+            for _ in 0...10 {
+                if let item = cartOrder.last {
+                    context.delete(item)
+                }
+                try!
+                context.save()
+                fetchDataFromDB()
+            }
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +47,8 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
         cartItemTableView.delegate = self
         cartItemTableView.dataSource = self
         fetchDataFromDB()
+        cleanDB()
+        super.viewDidLoad()
     }
     
     override func viewWillAppear(_ animated: Bool){
@@ -50,4 +62,32 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
             cartItemTableView.reloadData()
         }
     }
+    
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let actionDelete = UIContextualAction(style: .destructive, title: "Delete") { (actionDelete, view,completionHandler) in
+            
+            let itemToDelet = self.cartOrder[indexPath.row]
+            
+            self.context.delete(itemToDelet)
+            do{
+                try self.context.save()
+                
+            } catch {
+                
+            }
+            self.fetchDataFromDB()
+        }
+        return UISwipeActionsConfiguration (actions: [actionDelete])
+    }
+    
+    func cleanDB (){
+        let clean = NSBatchDeleteRequest(fetchRequest: FlowerInfo.fetchRequest())
+        do {try!
+            context.execute(clean)} }
+    
 }
+
+
+//NSBatchDeleteRequest(fetchRequest: cartOrder.fetchRequest())
